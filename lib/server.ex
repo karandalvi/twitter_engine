cf46109781f8e3f7fe9e96a9281877e16026a8d9
Karan Dalvi <karan.j.dalvi@gmail.com>
2017-11-28 02:48:39 -0500

    server running 32 engine processes

--- lib/server.ex
+++ lib/server.ex
@@ -6,8 +6,11 @@ defmodule Server do
 
     def init do
         db = Database.start
-        {:ok, engine} = Engine.start_link(db)
-        loop(engine)
+        engines = (for x <- 0..31 do
+            {:ok, engine} = Engine.start_link(db)    
+            engine
+        end)
+        loop(engines)
     end
 
     def loop(engine) do
@@ -18,7 +21,8 @@ defmodule Server do
     end
 
     defp process({:connect, caller}, engine) do
-        send caller, {:response, engine}
+        id = :rand.uniform(32)
+        send caller, {:response, Enum.at(engine, id-1)}
     end
 
 end
